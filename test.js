e99e307beba218f9480b1238320fd4ac92d60a40
Praveen Nandagiri <praveen.nandagiri@solsys.ca>
2021-06-09 23:21:26 -0400

    README updates

--- test.js
+++
@@ -1,9 +0,0 @@
-/// <reference> node.d.ts
-var {addJsonUrls, generateSiteMap} = require('./index');
-var data = require('./sitemap-data.json');
-console.log('validation:', addJsonUrls(data));
-var sitemap = generateSiteMap({
-    domain: 'https://dashboard.covid19fighters.page',
-    styleSheet: "default.xsl"
-});
-console.log(sitemap);
